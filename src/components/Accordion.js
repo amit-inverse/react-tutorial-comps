03642628d247fb26f4092abd11ab627d7557d3fb
Amit Kumar Das <amit.inverse@gmail.com>
2022-12-11 20:51:46 +0530

    Inline Evnet Handlers

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,16 +1,14 @@
 import { useState } from 'react';
 
 function Accordion({ items }) {
-    const [expandedIndex, setExpandedIndex] = useState(1);
+    const [expandedIndex, setExpandedIndex] = useState(0);
 
     const renderedItems = items.map((item, index) => {
         const isExpanded = index === expandedIndex;
 
-        // const content = isExpanded && <div>{item.content}</div>;
-
         return (
             <div key={item.id}>
-                <div>{item.label}</div>
+                <div onClick={() => setExpandedIndex(index)}>{item.label}</div>
                 {isExpanded && <div>{item.content}</div>}
             </div>
         );
