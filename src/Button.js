6103ce0de46e988a27ba5cac6eff7099dd094a78
Amit Kumar Das <amit.inverse@gmail.com>
2022-12-11 09:49:08 +0530

    The ClassNames Library

--- src/Button.js
+++ src/Button.js
@@ -1,4 +1,12 @@
-import PropTypes from 'prop-types';
+import className from 'classnames';
+
+// const finalClassName = className('px-3', 'py-1.5', 'bg-blue-500');
+// console.log(finalClassName);
+const finalClassName = className('px-1.5', {
+    'bg-blue-500': true,
+    'text-yellow-500': false,
+});
+console.log(finalClassName);
 
 function Button({ children, primary, secondary, success, warning, danger, outline, rounded }) {
     return <button className="px-3 py-1.5 border border-blue-600 bg-blue-500 text-white">{children}</button>;
