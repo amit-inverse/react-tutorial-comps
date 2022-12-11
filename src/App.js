5163685382e0a4c1de6be9fcb8e122f257157739
Amit Kumar Das <amit.inverse@gmail.com>
2022-12-11 17:19:30 +0530

    Finding the Expanded Item

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import Accordion from './components/Accordion';
 function App() {
     const items = [
         {
+            id: 'wrjjwe',
             label: 'Can I use React on a project?',
             content: 'You can use React. You can use React. You can use React.',
         },
         {
+            id: 'eterg',
             label: 'Can I use JavaScript on a project?',
-            content: '',
+            content: 'You can use React. You can use React. You can use React. You can use React. You can use React. You can use React.',
         },
         {
+            id: 'rtr43',
             label: 'Can I use CSS on a project?',
-            content: '',
+            content: 'You can use React. You can use React. You can use React.',
         },
     ];
 
--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,5 +1,26 @@
-function Accordion({ item }) {
-    return <div></div>;
+import { useState } from 'react';
+
+function Accordion({ items }) {
+    const [expandedIndex, setExpandedIndex] = useState(0);
+
+    const renderedItems = items.map((item, index) => {
+        // if (index === expandedIndex) {
+        //     console.log('Expanded');
+        // } else {
+        //     console.log('Collapsed');
+        // }
+        const isExpanded = index === expandedIndex;
+        // console.log(isExpanded);
+
+        return (
+            <div key={item.id}>
+                <div>{item.label}</div>
+                <div>{item.content}</div>
+            </div>
+        );
+    });
+
+    return <div>{renderedItems}</div>;
 }
 
 export default Accordion;
