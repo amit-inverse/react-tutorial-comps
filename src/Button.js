c0ea2d096fca21108a65c64e10bf71d7ba291bfe
Amit Kumar Das <amit.inverse@gmail.com>
2022-12-11 10:03:21 +0530

    Finalizing the VAriabtions

--- src/Button.js
+++ src/Button.js
@@ -7,6 +7,13 @@ function Button({ children, primary, secondary, success, warning, danger, outlin
         'border-green-500 bg-green-500 text-white': success,
         'border-yellow-400 bg-yellow-400 text-white': warning,
         'border-red-500 bg-red-500 text-white': danger,
+        'rounded-full': rounded,
+        'bg-white': outline,
+        'text-blue-500': outline && primary,
+        'text-gray-900': outline && secondary,
+        'text-green-500': outline && success,
+        'text-yellow-400': outline && warning,
+        'text-red-500': outline && danger,
     });
 
     return <button className={classes}>{children}</button>;
