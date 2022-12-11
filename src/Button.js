ca8f541299800ef3783ed531e661e2549db9dbdc
Amit Kumar Das <amit.inverse@gmail.com>
2022-12-11 09:20:48 +0530

    Review on Styling

--- src/Button.js
+++ src/Button.js
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 
 function Button({ children, primary, secondary, success, warning, danger, outline, rounded }) {
-    return <button>{children}</button>;
+    return <button className="px-3 py-1.5 border border-blue-600 bg-blue-500 text-white">{children}</button>;
 }
 
 Button.propTypes = {
