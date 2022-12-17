0f43a6079c3e818cb919c8e57c2b0e485e202bd6
Amit Kumar Das <amit.inverse@gmail.com>
2022-12-17 10:22:15 +0530

    Creating the Reusuable Panel

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { GoChevronDown } from 'react-icons/go';
+import Panel from './Panel';
 
 function Dropdown({ options, value, onChange }) {
     const [isOpen, setIsOpen] = useState(false);
@@ -25,11 +26,11 @@ function Dropdown({ options, value, onChange }) {
 
     return (
         <div className="w-48 relative">
-            <div className="flex justify-between items-center cursor-pointer border rounded p-3 shadow bg-white w-full" onClick={handleClick}>
+            <Panel className="flex justify-between items-center cursor-pointer" onClick={handleClick}>
                 {value?.label || 'Select...'}
-                <GoChevronDown className='text-lg' />
-            </div>
-            {isOpen && <div className="absolute top-full boder rounded p-3 shadow  bg-white w-full">{renderedOptions}</div>}
+                <GoChevronDown className="text-lg" />
+            </Panel>
+            {isOpen && <Panel className="absolute top-full">{renderedOptions}</Panel>}
         </div>
     );
 }
