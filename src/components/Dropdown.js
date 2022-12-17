9da8ce4114de71f9ecfe9a8bb4d22da924455e84
Amit Kumar Das <amit.inverse@gmail.com>
2022-12-17 08:50:58 +0530

    Finally, Implementation

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,5 +1,22 @@
+import { useState } from 'react';
+
 function Dropdown({ options }) {
-    return <div>Dropdown</div>;
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleClick = () => {
+        setIsOpen((currentIsOpen) => !currentIsOpen);
+    };
+
+    const renderedOptions = options.map((option) => {
+        return <div key={option.value}>{option.label}</div>;
+    });
+
+    return (
+        <div>
+            <div onClick={handleClick}>Select...</div>
+            {isOpen && <div>{renderedOptions}</div>}
+        </div>
+    );
 }
 
 export default Dropdown;
