121087fa50f423dcd28f402bf28c372e32ad5b2d
Amit Kumar Das <amit.inverse@gmail.com>
2022-12-18 11:53:26 +0530

    Link Styling

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,16 +1,26 @@
+import classNames from 'classnames';
 import { useContext } from 'react';
 import NavigationContext from '../context/navigation';
 
 function Link({ to, children }) {
     const { navigate } = useContext(NavigationContext);
 
+    const classes = classNames('text-blue-500');
+
     const handleClick = (event) => {
+        if (event.metaKey || event.ctrlKey) {
+            return;
+        }
         event.preventDefault();
 
         navigate(to);
     };
 
-    return <a onClick={handleClick}>{children}</a>;
+    return (
+        <a className={classes} href={to} onClick={handleClick}>
+            {children}
+        </a>
+    );
 }
 
 export default Link;
