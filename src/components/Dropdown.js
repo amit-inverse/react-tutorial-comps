c25d74b7f58f58b24d35c284ad1c92ecb8a78c72
Amit Kumar Das <amit.inverse@gmail.com>
2022-12-18 09:34:00 +0530

    useRef in Action

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,13 +1,14 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { GoChevronDown } from 'react-icons/go';
 import Panel from './Panel';
 
 function Dropdown({ options, value, onChange }) {
     const [isOpen, setIsOpen] = useState(false);
+    const divEl = useRef();
 
     useEffect(() => {
         const handler = (event) => {
-            console.log(event.target);
+            console.log(divEl.current);
         };
 
         document.addEventListener('click', handler, true);
@@ -37,7 +38,7 @@ function Dropdown({ options, value, onChange }) {
     });
 
     return (
-        <div className="w-48 relative">
+        <div ref={divEl} className="w-48 relative">
             <Panel className="flex justify-between items-center cursor-pointer" onClick={handleClick}>
                 {value?.label || 'Select...'}
                 <GoChevronDown className="text-lg" />
