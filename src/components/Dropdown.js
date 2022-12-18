25d4f28028410fc83f624e5c404b7c3e50af0f31
Amit Kumar Das <amit.inverse@gmail.com>
2022-12-18 09:38:07 +0530

    Checking Click Location

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -8,7 +8,12 @@ function Dropdown({ options, value, onChange }) {
 
     useEffect(() => {
         const handler = (event) => {
-            console.log(divEl.current);
+            if (!divEl.current) {
+                return;
+            }
+            if (!divEl.current.contains(event.target)) {
+                setIsOpen(false);
+            }
         };
 
         document.addEventListener('click', handler, true);
