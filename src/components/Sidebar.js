5cd27260372b60b8d715d6661cd5c537764eca03
Amit Kumar Das <amit.inverse@gmail.com>
2022-12-18 13:02:43 +0530

    Highlighting the Active Link

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -9,13 +9,13 @@ function Sidebar() {
 
     const renderedLinks = links.map((link) => {
         return (
-            <Link key={link.label} to={link.path}>
+            <Link key={link.label} to={link.path} className="mb-3" activeClassName="font-bold border-l-4 border-blue-500 pl-2">
                 {link.label}
             </Link>
         );
     });
 
-    return <div className="sticky top-0 overflow-y-auto flex flex-col">{renderedLinks}</div>;
+    return <div className="sticky top-0 overflow-y-auto flex flex-col items-start">{renderedLinks}</div>;
 }
 
 export default Sidebar;
