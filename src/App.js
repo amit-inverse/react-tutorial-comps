ac98919c42f4190f96dc29289061fb137cf1a5c9
Amit Kumar Das <amit.inverse@gmail.com>
2022-12-18 11:39:19 +0530

    A Link Component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import Link from './components/Link';
+
 function App() {
-    return <div></div>;
+    return (
+        <div>
+            <Link to="/accordion">Go to accordion</Link>
+            <Link to="/dropdown">Go to dropdown</Link>
+        </div>
+    );
 }
 
 export default App;
