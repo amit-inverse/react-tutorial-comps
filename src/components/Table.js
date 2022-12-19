353d2132e149750f33d98626aa0fbafe8968bd67
Amit Kumar Das <amit.inverse@gmail.com>
2022-12-19 21:37:51 +0530

    Building the Rows

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,4 +1,14 @@
 function Table({ data }) {
+    const renderedRows = data.map((fruit) => {
+        return (
+            <tr key={fruit.name}>
+                <td>{fruit.name}</td>
+                <td>{fruit.color}</td>
+                <td>{fruit.score}</td>
+            </tr>
+        );
+    });
+
     return (
         <table>
             <thead>
@@ -8,7 +18,7 @@ function Table({ data }) {
                     <th>Score</th>
                 </tr>
             </thead>
-            <tbody></tbody>
+            <tbody>{renderedRows}</tbody>
         </table>
     );
 }
