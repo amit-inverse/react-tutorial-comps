3df5b660370361ff0c415dc6ad4af053c042c2c6
Amit Kumar Das <amit.inverse@gmail.com>
2022-12-22 21:59:15 +0530

    Yesss... It Worked

--- src/components/SortableTable.js
+++ src/components/SortableTable.js
@@ -1,10 +1,11 @@
+import { type } from '@testing-library/user-event/dist/type';
 import { useState } from 'react';
 import Table from './Table';
 
 function SortableTable(props) {
     const [sortOrder, setSortOrder] = useState(null);
     const [sortBy, setSortBy] = useState(null);
-    const { config } = props;
+    const { config, data } = props;
 
     const handleClick = (label) => {
         if (sortOrder === null) {
@@ -30,10 +31,30 @@ function SortableTable(props) {
         };
     });
 
+    // Only sort data if sorOrder && sortBy are not null
+    // Make a copy of the 'data' prop
+    // Find the correct sortValue function and use it for sorting
+    let sortedData = data;
+    if (sortOrder && sortBy) {
+        const { sortValue } = config.find((column) => column.label === sortBy);
+        sortedData = [...data].sort((a, b) => {
+            const valueA = sortValue(a);
+            const valueB = sortValue(b);
+
+            const reverseOrder = sortOrder === 'asc' ? 1 : -1;
+
+            if (typeof valueA === 'string') {
+                return valueA.localeCompare(valueB) * reverseOrder;
+            } else {
+                return (valueA - valueB) * reverseOrder;
+            }
+        });
+    }
+
     return (
         <div>
             {sortOrder} - {sortBy}
-            <Table {...props} config={updatedConfig} />
+            <Table {...props} data={sortedData} config={updatedConfig} />
         </div>
     );
 }
