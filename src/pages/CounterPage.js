33326dfcd538d1a139952f78035192e8e7292b50
Amit Kumar Das <amit.inverse@gmail.com>
2022-12-24 12:09:24 +0530

    UseReducer in Action

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -1,35 +1,44 @@
-import { useState } from 'react';
+// import { useState } from 'react';
+import { useReducer } from 'react';
 import Button from '../components/Button';
 import Panel from '../components/Panel';
 
+const reducer = (state, action) => {
+    //
+};
+
 function CounterPage({ initialCount }) {
-    const [count, setCount] = useState(initialCount);
-    const [valueToAdd, setValueToAdd] = useState(0);
+    // const [count, setCount] = useState(initialCount);
+    // const [valueToAdd, setValueToAdd] = useState(0);
+    const [state, dispatch] = useReducer(reducer, {
+        count: initialCount,
+        valueToAdd: 0,
+    });
 
     const increment = () => {
-        setCount(count + 1);
+        // setCount(count + 1);
     };
 
     const decrement = () => {
-        setCount(count - 1);
+        // setCount(count - 1);
     };
 
     const handleChange = (event) => {
         const value = parseInt(event.target.value) || 0;
 
-        setValueToAdd(value);
+        // setValueToAdd(value);
     };
 
     const handleSubmit = (event) => {
         event.preventDefault();
 
-        setCount(count + valueToAdd);
-        setValueToAdd(0);
+        // setCount(count + valueToAdd);
+        // setValueToAdd(0);
     };
 
     return (
         <Panel className="m-3">
-            <h1 className="text-lg">Count is {count}</h1>
+            <h1 className="text-lg">Count is {state.count}</h1>
             <div className="flex flex-row">
                 <Button onClick={increment}>Increment</Button>
                 <Button onClick={decrement}>Decrement</Button>
@@ -37,7 +46,7 @@ function CounterPage({ initialCount }) {
 
             <form onSubmit={handleSubmit}>
                 <label>Add a lot!</label>
-                <input value={valueToAdd || ''} onChange={handleChange} type="number" className="p-1 m-3 bg-gray-50 border border-gray-300" />
+                <input value={state.valueToAdd || ''} onChange={handleChange} type="number" className="p-1 m-3 bg-gray-50 border border-gray-300" />
                 <Button>Add it!</Button>
             </form>
         </Panel>
