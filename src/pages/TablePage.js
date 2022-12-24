a9914e92f4763305c8fe92c1575f76ed0110d931
Amit Kumar Das <amit.inverse@gmail.com>
2022-12-24 08:22:50 +0530

    Creating the Demo Component

--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -1,4 +1,4 @@
-import Table from '../components/Table';
+// import Table from '../components/Table';
 import SortableTable from '../components/SortableTable';
 
 function TablePage() {
@@ -7,6 +7,7 @@ function TablePage() {
         { name: 'Apple', color: 'bg-red-500', score: 3 },
         { name: 'Banana', color: 'bg-yellow-500', score: 1 },
         { name: 'Lime', color: 'bg-green-500', score: 4 },
+        { name: 'Cherry', color: 'bg-red-700', score: 2.5 },
     ];
 
     const config = [
@@ -24,6 +25,11 @@ function TablePage() {
             render: (fruit) => fruit.score,
             sortValue: (fruit) => fruit.score,
         },
+        {
+            label: 'Score Squared',
+            render: (fruit) => fruit.score ** 2,
+            sortValue: (fruit) => fruit.score ** 2,
+        },
     ];
 
     const keyFn = (fruit) => {
@@ -32,6 +38,7 @@ function TablePage() {
 
     return (
         <div>
+            {/* <Table data={data} config={config} keyFn={keyFn} /> */}
             <SortableTable data={data} config={config} keyFn={keyFn} />
         </div>
     );
