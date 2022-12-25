fdb97ce8aecb49751f15eb8a40c2de69bd07ed9c
Amit Kumar Das <amit.inverse@gmail.com>
2022-12-25 08:32:39 +0530

    Adding New State Updates

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -4,7 +4,9 @@ import Button from '../components/Button';
 import Panel from '../components/Panel';
 
 const INCREMENT_COUNT = 'increment';
+const DECREMENT_COUNT = 'decrement';
 const SET_VALUE_TO_ADD = 'set-value-to-add';
+const ADD_VALUE_TO_COUNT = 'add-value-to-count';
 
 const reducer = (state, action) => {
     switch (action.type) {
@@ -13,10 +15,21 @@ const reducer = (state, action) => {
                 ...state,
                 count: state.count + 1,
             };
+        case DECREMENT_COUNT:
+            return {
+                ...state,
+                count: state.count - 1,
+            };
         case SET_VALUE_TO_ADD:
             return {
                 ...state,
                 valueToAdd: action.payload,
+            };
+        case ADD_VALUE_TO_COUNT:
+            return {
+                ...state,
+                count: state.count + state.valueToAdd,
+                valueToAdd: 0,
             };
         default:
             return state;
@@ -24,22 +37,21 @@ const reducer = (state, action) => {
 };
 
 function CounterPage({ initialCount }) {
-    // const [count, setCount] = useState(initialCount);
-    // const [valueToAdd, setValueToAdd] = useState(0);
     const [state, dispatch] = useReducer(reducer, {
         count: initialCount,
         valueToAdd: 0,
     });
 
     const increment = () => {
-        // setCount(count + 1);
         dispatch({
             type: INCREMENT_COUNT,
         });
     };
 
     const decrement = () => {
-        // setCount(count - 1);
+        dispatch({
+            type: DECREMENT_COUNT,
+        });
     };
 
     const handleChange = (event) => {
@@ -54,8 +66,9 @@ function CounterPage({ initialCount }) {
     const handleSubmit = (event) => {
         event.preventDefault();
 
-        // setCount(count + valueToAdd);
-        // setValueToAdd(0);
+        dispatch({
+            type: ADD_VALUE_TO_COUNT,
+        });
     };
 
     return (
